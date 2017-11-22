8b2393ab669984aaecd392f309670051aba782cb
cyij <cyij2006@gmail.com>
2017-11-22 22:33:51 +0800

    up

--- template/src/pages/Index.vue
+++ template/src/pages/Index.vue
@@ -1,28 +1,26 @@
 <template>
-  <div>
+<div>
   <mt-field label="邮箱" state="success" v-model="email"></mt-field>
   <mt-field label="邮箱" state="error" v-model="email"></mt-field>
   <mt-field label="邮箱" state="warning" v-model="email"></mt-field>
-  </div>
+  <mt-cell title="goto home" is-link to="home?a=1"></mt-cell>
+</div>
 </template>
 <script>
 {{#if_eq dependency "byneed"}}
-import {Field} from 'mint-ui'
+import {Field, Cell} from 'mint-ui'
 {{/if_eq}}
 export default {
   {{#if_eq dependency "byneed"}}
   components: {
-    Field.name: Field
+    Field.name: Field,
+    Cell.name: Cell
   },
   {{/if_eq}}
   data () {
     return {
-      email: '',
-      pname: ''
+      email: ''
     }
   }
 }
 </script>
-<style lang="less" scoped>
-span {color: red;}
-</style>
