f55ca16a6c44996215d5daca98c6c2980242ba73
cyij <cyij2006@gmail.com>
2017-11-29 23:21:48 +0800

    up

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,8 +1,24 @@
 <template>
-<div id="app">
-  <mt-button size="small" type="primary" @click.native="$router.goto('index')">Index</mt-button>
-  <mt-button size="small" type="danger" @click.native="$router.goto('home')">Home</mt-button>
-  <mt-button size="small" type="primary" @click.native="$router.goto('my')">My</mt-button>
-  <page-view></page-view>
-</div>
+  <div id="app">
+    <mt-button size="small" type="primary" @click.native="$router.goto('index')">Index</mt-button>
+    <mt-button size="small" type="danger" @click.native="$router.goto('home')">Home</mt-button>
+    <page-view/>
+  </div>
 </template>
+
+<script>
+export default {
+  created () {
+    window.abc = this
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
--- template/src/entry-client.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import {createApp} from './app'
-import 'mint-ui/lib/style.css'
-
-const { app, router} = createApp()
-
-router.onReady(() => {
-  app.$mount('#app')
-})
