2e5e765c56fc0449e45bf9a7762bdc90bf22bed7
cyij <cyij2006@gmail.com>
2017-11-29 23:23:24 +0800

    up

--- template/src/App.vue
+++ template/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <mt-button size="small" type="primary" @click.native="$router.goto('index')">Index</mt-button>
     <mt-button size="small" type="danger" @click.native="$router.goto('home')">Home</mt-button>
+    <mt-button size="small" type="primary" @click.native="$router.goto('my')">My</mt-button>
     <page-view/>
   </div>
 </template>
